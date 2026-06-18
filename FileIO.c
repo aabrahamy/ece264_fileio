175b5fab7869643b1fb4a91fcc91776267f450a2
Ani Abrahamyan <aniabrahamyan800@gmail.com>
2026-06-18 18:55:35 -0400

    Add files via upload

---
+++ FileIO.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(int argc, char ** argv){
+    int sum = 0;
+    int num = 0;
+
+    if (argc != 2){
+        printf("I need the filename next time.");
+        return 1;
+    }
+
+    //Complete fopen argument to open file in read mode
+    FILE * numptr = fopen(argv[1], ___);
+
+    if (numptr == NULL){
+        return 1;
+    }
+
+    //Complete while loop condition to read an integer from file
+    while (__________){
+        sum +=num;
+    }
+
+    printf("%d\n", sum);
+
+    //Write line to close file
+    
+    
+    return 0;
+}
