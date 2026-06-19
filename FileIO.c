cea83cb4d8039eb24df108b60c24227db0afbdbe
Ani Abrahamyan <abrahamy@ececomp2.ecn.purdue.edu>
2026-06-18 20:16:17 -0400

    completed

--- FileIO.c
+++ FileIO.c
@@ -10,20 +10,22 @@ int main(int argc, char ** argv){
     }
 
     //Complete fopen argument to open file in read mode
-    FILE * numptr = fopen(argv[1], ___);
+    FILE * numptr = fopen(argv[1], "r");
 
     if (numptr == NULL){
         return 1;
     }
 
     //Complete while loop condition to read an integer from file
-    while (__________){
+    // Asking for one integer value, so checks == 1
+    while (fscanf(numptr, "%d", &num) == 1){
         sum +=num;
     }
 
     printf("%d\n", sum);
 
     //Write line to close file
+    fclose(numptr);
     
     
     return 0;
